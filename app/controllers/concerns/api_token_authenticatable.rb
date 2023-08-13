48e77be6637a108c679b0277c7b3234ef6927b16
rhebeca abreu <rhebecaabreu@gmail.com>
2023-08-13 14:16:25 -0300

    remove unnecessary code

--- app/controllers/concerns/api_token_authenticatable.rb
+++ app/controllers/concerns/api_token_authenticatable.rb
@@ -31,13 +31,6 @@ module ApiTokenAuthenticatable
     request_email = request.headers.env["HTTP_#{header_name(model)}_EMAIL"].presence
     request_token = request.headers.env["HTTP_#{header_name(model)}_TOKEN"].presence
 
-
-    Rails.logger.info("\n\n INFO ---- #{request_email} \n\n ")
-    Rails.logger.info("\n\n INFO ---- #{request_token} \n\n ")
-    Rails.logger.info("\n\n INFO ---- #{request.headers.env} \n\n ")
-    Rails.logger.info("\n\n INFO ---- #{request.headers["HTTP_USER_EMAIL"]} \n\n ")
-    Rails.logger.info("\n\n INFO ---- #{request.headers["HTTP_USER_TOKEN"]} \n\n ")
-
     user = request_email && model.find_by(email: request_email)
     [user, request_token]
   end
