e4b160b475a51e9a01c144b79c76b31dd03ac67e
rhebeca abreu <rhebecaabreu@gmail.com>
2023-08-22 00:10:28 -0300

    Fix image upload

--- app/controllers/api/publications_controller.rb
+++ app/controllers/api/publications_controller.rb
@@ -8,24 +8,27 @@ class Api::PublicationsController < ApplicationController
   end
 
   def create
-    category = Category.find_by(id: publication_params[:category_id])
+    address = JSON.parse(params[:address])
+    pet = JSON.parse(params[:pet])
+    publish_params = publication_params.merge(address: address, pet: pet)
+
+    category = Category.find_by(id: publish_params[:category_id])
 
     if category.present?
       publication = current_user.publications.create(
-        title: publication_params[:title], description: publication_params[:description], category: category
+        title: publish_params[:title], description: publish_params[:description], category: category
       )
-
       if publication.present?
-        address = Address.create(addressable: publication, state: publication_params[:address][:state], city: publication_params[:address][:city],
-                                 cep: publication_params[:address][:cep], street: publication_params[:address][:street], number: publication_params[:address][:number], complement: publication_params[:address][:complement])
+        address = Address.create(addressable: publication, state: publish_params[:address][:state], city: publish_params[:address][:city],
+                                 cep: publish_params[:address][:cep], street: publish_params[:address][:street], number: publish_params[:address][:number], complement: publish_params[:address][:complement])
         pet = Pet.create(
-          publication: publication, kind: publication_params[:pet][:kind], age: publication_params[:pet][:age],
-          vaccinated: publication_params[:pet][:vaccinated], castrated: publication_params[:pet][:castrated], vermifugated: publication_params[:pet][:vermifugated],
-          coat_color: publication_params[:pet][:coat_color], status: publication_params[:pet][:status], size: publication_params[:pet][:size])
+          publication: publication, kind: publish_params[:pet][:kind], age: publish_params[:pet][:age],
+          vaccinated: publish_params[:pet][:vaccinated], castrated: publish_params[:pet][:castrated], vermifugated: publish_params[:pet][:vermifugated],
+          coat_color: publish_params[:pet][:coat_color], status: publish_params[:pet][:status], size: publish_params[:pet][:size])
 
         if pet.present?
-          publication_params[:pet][:images].each do |image|
-            Image.create(remote_file_url: image, imageable: pet)
+          publish_params[:images].each do |key, image|
+            Image.create(file: image, imageable: pet)
           end
         end
 
@@ -33,7 +36,7 @@ class Api::PublicationsController < ApplicationController
 
         render_success(publication)
       else
-        render_not_found_error
+        render_unprocessable_entity_error(publication.errors)
       end
     else
       render_not_found_error
@@ -44,7 +47,8 @@ class Api::PublicationsController < ApplicationController
 
   def publication_params
     params.permit(:title, :description, :category_id,
-                  pet: [:kind, :age, :vaccinated, :castrated, :vermifugated, :coat_color, :status, :size, :sex, images: []],
+                  images: {},
+                  pet: [:kind, :age, :vaccinated, :castrated, :vermifugated, :coat_color, :status, :size, :sex ],
                   address: [:state, :city, :cep, :street, :number, :complement])
   end
 
