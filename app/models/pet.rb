de1eea5055cb11fa30cb7c8d8fec3da98461eb2f
rhebeca abreu <rhebecaabreu@gmail.com>
2023-10-12 22:10:04 -0300

    Fix pet validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,11 +6,11 @@ class Pet < ApplicationRecord
   has_many :images, as: :imageable
 
   validates :kind, :age, :coat_color, :status, :size, :sex, presence: true
-  validates :vaccinated, :castrated, :vermifugated, inclusion: [true, false]
+  # validates :vaccinated, :castrated, :vermifugated, inclusion: [true, false]
 
   enumerize :status, in: [:looking_home, :found_home], default: :looking_home
   enumerize :kind, in: [:dog, :cat], default: :cat
   enumerize :size, in: [:small, :medium, :big], default: :small
-  enumerize :sex, in: [:male, :female, :undefined], default: :female
+  enumerize :sex, in: [:male, :female, :unknown], default: :female
 
 end
